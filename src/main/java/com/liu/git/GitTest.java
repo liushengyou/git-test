ce9e07846c28626e205204e3044e670fcb8aeba9
liushengyou <112490026+liushengyou@users.noreply.github.com>
2022-08-31 21:28:22 +0800

    pull commit

--- src/main/java/com/liu/git/GitTest.java
+++ src/main/java/com/liu/git/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
         System.out.println("hello git4");
         System.out.println("master commit");
         System.out.println("hot-fix commit");
+        System.out.println("pull test");
     }
 
 }
