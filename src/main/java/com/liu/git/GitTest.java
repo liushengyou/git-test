ff419c62e421f00c90610d53081f58ab973d09dc
Liu <1315802483@qq.com>
2022-08-31 20:12:38 +0800

    second commit

--- src/main/java/com/liu/git/GitTest.java
+++ src/main/java/com/liu/git/GitTest.java
@@ -3,7 +3,8 @@ package com.liu.git;
 public class GitTest {
 
     public static void main(String[] args) {
-        System.out.println("hello git");
+        System.out.println("hello git1");
+        System.out.println("hello git2");
     }
 
 }
